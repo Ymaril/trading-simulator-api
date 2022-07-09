86f7052ea1672f69c6449a7e6bc7d315cc3019ef
Aleksey Timofeev <a.timofeev@vmb-team.com>
2022-07-10 00:06:10 +0300

    Add order cancel service

---
+++ app/services/orders/cancel_service.rb
@@ -0,0 +1,20 @@
+module Orders
+  class CancelService < ::CrudServices::Save
+    def initialize(object, current_user = nil)
+      @object = object
+      @current_user = current_user
+    end
+
+    private
+
+    def save_object
+      object.cancel
+
+      if object.save
+        success_result data: { object_name => object }
+      else
+        invalid_params_result object
+      end
+    end
+  end
+end
---
+++ spec/services/orders/charge_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Orders::CancelService, type: :service do
+  subject { described_class.perform(order, current_user) }
+
+  let(:current_user) { create(:user) }
+  let(:order) { create(:order, user: current_user, state: :created) }
+
+  it do
+    expect(subject.success?).to be_truthy
+    expect(order.reload).to be_canceled
+  end
+end
