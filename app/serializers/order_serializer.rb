5c86cfc010f2a84dc772502a245cf07c217a26fe
Aleksey Timofeev <a.timofeev@vmb-team.com>
2022-07-10 01:46:39 +0300

    Add order serializer

---
+++ app/serializers/order_serializer.rb
@@ -0,0 +1,8 @@
+class OrderSerializer < Blueprinter::Base
+  identifier :id
+
+  fields :value, :expires_at, :completed_at, :state, :complete_type
+
+  association :from_currency, blueprint: CurrencySerializer
+  association :to_currency, blueprint: CurrencySerializer
+end
---
+++ spec/serializers/order_serializer_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe OrderSerializer, type: :serializer do
+  subject { described_class.render_as_hash(order) }
+
+  let(:now) { DateTime.now }
+
+  let(:from_currency) { create(:currency, name: 'Dollar', code: 'USD') }
+  let(:to_currency) { create(:currency, name: 'Ruble', code: 'RUB') }
+  let(:order) do
+    create(
+      :order, 
+      from_currency: from_currency, 
+      to_currency: to_currency,
+      value: 10,
+      expires_at: now + 15.minutes,
+      completed_at: nil,
+      complete_type: :take_profit
+    ) 
+  end
+
+  it do
+    is_expected.to match(
+      value: 10,
+      complete_type: 'take_profit',
+      completed_at: nil,
+      state: :created,
+      from_currency: {code: 'USD', name: 'Dollar', id: from_currency.id},
+      to_currency: {code: 'RUB', name: 'Ruble', id: to_currency.id},
+      expires_at: now + 15.minutes,
+      id: order.id
+    )
+  end
+end
