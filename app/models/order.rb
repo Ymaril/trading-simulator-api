015b1e32c3e9afa53963d8c50c4ef7a65d5300e7
Aleksey Timofeev <a.timofeev@vmb-team.com>
2022-07-10 01:46:26 +0300

    Fux state enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
   validates :user, :from_currency, :to_currency, :value,  presence: true
   validates :value, numericality: {greater_than_or_equal_to: 0}
 
-  enum complete_type: %w[take_profit stop_loss]
+  enum complete_type: {take_profit: 'take_profit', stop_loss: 'stop_loss'}
   
   state_machine initial: :created do
     event :complete do
