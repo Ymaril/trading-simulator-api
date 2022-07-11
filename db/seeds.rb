d6988964f4661ac3b1d1a1820a8ccf4248db169c
Aleksey Timofeev <a.timofeev@vmb-team.com>
2022-07-12 00:17:50 +0300

    Create admin user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'admin@example.com', password: '12345', admin: true)
